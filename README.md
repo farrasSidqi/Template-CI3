4b72959a602388e5cf1533bb0fbc5670aeba268d
Lana Bang <r00tdeveloper7@gmail.com>
2024-09-30 01:45:31 +0700

    update baru

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Template-CI3
+
+Konsep dirancang untuk memberikan kemudahan dalam pengembangan aplikasi silahkan tarik terlebih dahulu
+dan siap untuk di gunakan.
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -3,32 +3,32 @@
 		        <li class="nav-item nav-profile border-bottom">
 		            <a href="#" class="nav-link flex-column">
 		                <div class="nav-profile-image">
-		                    <img src="../assets/images/faces/face1.jpg" alt="profile" />
+		                    <img src="<?php echo base_url();?>/assets/images/faces/face1.jpg" alt="profile" />
 		                    <!--change to offline or busy as needed-->
 		                </div>
 		                <div class="nav-profile-text d-flex ms-0 mb-3 flex-column">
 		                    <span class="font-weight-semibold mb-1 mt-2 text-center">Antonio Olson</span>
-		                    <span class="text-secondary icon-sm text-center">$3499.00</span>
+		                    <span class="text-secondary icon-sm text-center">Sebagai</span>
 		                </div>
 		            </a>
 		        </li>
 		        <li class="nav-item pt-3">
 		            <a class="nav-link d-block" href="index.html">
-		                <img class="sidebar-brand-logo" src="../assets/images/logo.svg" alt="" />
-		                <img class="sidebar-brand-logomini" src="../assets/images/logo-mini.svg" alt="" />
+		                <img class="sidebar-brand-logo" src="<?php echo base_url();?>/assets/images/logo.svg" alt="" />
+		                <img class="sidebar-brand-logomini" src="<?php echo base_url();?>/assets/images/logo-mini.svg" alt="" />
 		                <div class="small font-weight-light pt-1">Responsive Dashboard</div>
 		            </a>
-		            <form class="d-flex align-items-center" action="#">
+		            <form class="d-flex align-items-center" action="#" method="post">
 		                <div class="input-group">
 		                    <div class="input-group-prepend">
 		                        <i class="input-group-text border-0 mdi mdi-magnify"></i>
 		                    </div>
-		                    <input type="text" class="form-control border-0" placeholder="Search" />
+		                    <input type="text" class="form-control border-0" placeholder="Cari rekening" />
 		                </div>
 		            </form>
 		        </li>
 		        <li class="pt-2 pb-1">
-		            <span class="nav-item-head">Template Pages</span>
+		            <span class="nav-item-head">Menu Utama</span>
 		        </li>
 		        <li class="nav-item">
 		            <a class="nav-link" href="index.html">
