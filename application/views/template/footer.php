7be0e5f397dd5f2d9da60735913850af2b20ecf2
Lana Bang <r00tdeveloper7@gmail.com>
2024-09-30 00:55:11 +0700

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,41 @@
+<!-- content-wrapper ends -->
+<!-- partial:partials/_footer.html -->
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021 <a href="https://www.bootstrapdash.com/" target="_blank">BootstrapDash</a>. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+    </div>
+</footer>
+<!-- partial -->
+</div>
+<!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- plugins:js -->
+<script src="<?php echo base_url(); ?>assets/vendors/js/vendor.bundle.base.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="<?php echo base_url(); ?>assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/chart.js/Chart.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/flot/jquery.flot.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/flot/jquery.flot.resize.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/flot/jquery.flot.categories.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/flot/jquery.flot.fillbetween.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendors/flot/jquery.flot.stack.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.cookie.js" type="text/javascript"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/misc.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/settings.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/todolist.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="<?php echo base_url(); ?>assets/js/dashboard.js"></script>
+<!-- End custom js for this page -->
+</body>
+
+</html>
